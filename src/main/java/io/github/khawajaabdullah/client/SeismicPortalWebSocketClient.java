a65144bdf85139a29e1d678bcbc7f145490b5c7a
khawaja-abdullah <khawaja.abdullah.ansar@gmail.com>
2026-06-27 13:14:08 +0500

    implement web socket client and connector for Seismic Portal live earthquake feed

---
+++ src/main/java/io/github/khawajaabdullah/client/SeismicPortalWebSocketClient.java
@@ -0,0 +1,57 @@
+package io.github.khawajaabdullah.client;
+
+import io.quarkus.runtime.StartupEvent;
+import io.quarkus.websockets.next.*;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
+import org.jboss.logging.Logger;
+
+import java.net.URI;
+
+@ApplicationScoped
+@WebSocketClient(path = "/standing_order/websocket")
+public class SeismicPortalWebSocketClient {
+
+  private static final Logger LOGGER = Logger.getLogger(SeismicPortalWebSocketClient.class);
+
+  @Inject
+  WebSocketConnector<SeismicPortalWebSocketClient> seismicPortalWebSocketClientWebSocketConnector;
+
+  void onStart(@Observes StartupEvent startupEvent) {
+    connect();
+  }
+
+  @OnOpen
+  void onOpen() {
+    LOGGER.info("Successfully opened connection to Seismic Portal live stream!");
+  }
+
+  @OnTextMessage
+  void onTextMessage(String message) {
+    LOGGER.infof("Message received from Seismic Portal live stream: %s", message);
+  }
+
+  @OnError
+  void onError(Throwable throwable) {
+    LOGGER.errorf("Connection to Seismic Portal live stream failed: %s", throwable.getMessage());
+    connect();
+  }
+
+  @OnClose
+  void onClose(CloseReason closeReason) {
+    LOGGER.warnf("Connection to Seismic Portal live stream closed: %s", closeReason.getMessage());
+    connect();
+  }
+
+  private void connect() {
+    seismicPortalWebSocketClientWebSocketConnector.baseUri(URI.create("wss://www.seismicportal.eu"))
+        .connect()
+        .subscribe()
+        .with(
+            webSocketClientConnection -> LOGGER.info("Successfully connected to Seismic Portal live stream!"),
+            throwable -> LOGGER.errorf("Connection to Seismic Portal live stream failed: %s", throwable.getMessage())
+        );
+  }
+
+}
